12bc34b266f642e01c1e1f7167b4de13338f3751
abdullahwaheed <pgcbwp12@gmail.com>
2023-06-30 16:23:10 +0500

    flutter upgrade 3.0.5

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:admin_bite/pages/404/error_page.dart';
 import 'package:admin_bite/routing/routes.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide MenuController;
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
